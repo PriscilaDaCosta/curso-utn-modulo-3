d5ee96afa721306de76000cc80a32dc65d6553d5
Priscila Da Costa <priscilatmv86@gmail.com>
2022-02-21 11:20:57 -0300

    se agrega ejecios 1, 2 y 3

---
+++ unidad 4 ejercicio 1/js/main.js
@@ -0,0 +1,12 @@
+function cambiarFondo(){
+    document.body.style.backgroundColor = '#ff3399'
+}
+
+function cambiarFondo2(){
+    document.body.style.backgroundColor = '#9966ff'
+}
+
+function cambiarFondo3(){
+    document.body.style.backgroundColor = '#3399ff'
+}
+
