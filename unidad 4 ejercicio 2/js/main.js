412c2c17d8a9669f573e0ef4b1871993a8434d5a
Priscila Da Costa <priscilatmv86@gmail.com>
2022-02-22 12:26:58 -0300

    se modifico ejercicio 2

--- unidad 4 ejercicio 2/js/main.js
+++ unidad 4 ejercicio 2/js/main.js
@@ -1,7 +1,13 @@
-function controlCaracteres() {
-    if (document.getElementById('control').value.length > 20) {
-        alert('Hasta 20 caracteres')
-    }else {
-        alert('Exitoso')
+const contar = document.querySelector("#contador");
+
+contar.addEventListener("keypress", function(i){
+    const inputLength = contar.value.length;
+    const maxCar = 30;
+
+    if(inputLength >= maxCar){
+        i.preventDefault();
+            alert("Caracteres maximos 30")
+    
     }
-}
+})
+
